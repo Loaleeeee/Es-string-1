80ab68536d754aa106ea2be79eec76e52172bdb0
Loaleeeee <morisialessio@gmail.com>
2025-01-22 12:48:38 +0100

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,18 +4,27 @@
 using namespace std;
 int main()
 {
+	
 	srand(time(0));
 	int inculata=0;
-	for(int i=0;i<6;i++)
+	int salvezza=0;
+	int N;
+	cout<<"Quanti voti generare:";
+	cin>>N;
+	for(int i=0;i<N;i++)
 	{
-		int num =rand()%6+1;
+		int num =rand()%10+1;
 		cout<<num<<endl;
-		if(num==1)
+		if(num<=5)
 			{
 				inculata++;
 			}
+		else
+			{
+				salvezza++;
+			}
 	}
-	cout<<"Sei stato inculato "<<inculata<<" volte";
-	
+	cout<<"N di salvataggi: "<<salvezza<<endl;
+	cout<<"N di inculate: "<<inculata;
+	return (0);
 }
-
